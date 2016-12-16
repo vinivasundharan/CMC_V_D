142ce3f5f129878935bd4388986c832c64e089c1
Vini <Vini@CMC-L-005>
2016-12-16 11:58:37 +0100

    Model update

--- DailyMonitoringCMC/Models/Trend.cs
+++ DailyMonitoringCMC/Models/Trend.cs
@@ -19,25 +19,21 @@ namespace DailyMonitoringCMC.Models
         public double F3Phase { get; set; }
         public double F4Phase { get; set; }
         public double F5Phase { get; set; }
-
         public double F1HA { get; set; }
         public double F2HA { get; set; }
         public double F3HA { get; set; }
         public double F4HA { get; set; }
         public double F5HA { get; set; }
-
         public double F1HW { get; set; }
         public double F2HW { get; set; }
         public double F3HW { get; set; }
         public double F4HW { get; set; }
         public double F5HW { get; set; }
-
         public int F1Status { get; set; }
         public int F2Status { get; set; }
         public int F3Status { get; set; }
         public int F4Status { get; set; }
         public int F5Status { get; set; }
-
         public double Speed { get; set; }
         public double Process { get; set; }
         public int Digital { get; set; }
